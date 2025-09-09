6ad66ccaa518fee4fba30746eb7f46284276a6bc
yukioroshi <148077899+yukioroshi@users.noreply.github.com>
2025-09-09 16:23:00 +0200

    test

--- Assets/Malfonction.cs
+++ Assets/Malfonction.cs
@@ -1,10 +1,12 @@
 using UnityEngine;
 using System.Collections.Generic;
 using Unity.VisualScripting;
+using UnityEngine.UI;
 
 public class Malfunction : MonoBehaviour
 {
-    //public Texture malfuctiontexture;
+    public RawImage CamOutput;
+    public Texture malfuctiontexture;
 
     public RenderTexture CamRenderTexture;
     public RenderTexture MalfuctionTexture;
@@ -15,7 +17,13 @@ public class Malfunction : MonoBehaviour
     public Camera cam3;
 
     [Range(0, 100)]
-    public int malfunctionValue = 100;
+    public int malfunctionValue1 = 100;
+
+    [Range(0, 100)]
+    public int malfunctionValue2 = 100;
+
+    [Range(0, 100)]
+    public int malfunctionValue3 = 100;
 
     void Start()
     {
@@ -24,16 +32,24 @@ public class Malfunction : MonoBehaviour
 
     void Update()
     {
-            if (malfunctionValue == 0)
-            {
-                cam1.targetTexture = MalfuctionTexture;
-                //cam1.enabled = false;   // Caméra coupée
-            }
-            else
-            {
-                //cam1.enabled = true;    // Caméra active
-                cam1.targetTexture = CamRenderTexture;
-            }
+
+    }
+
+
+    public void buttoncheckmalfunction()
+    {
+        if (malfunctionValue1 == 0)
+        {
+            //cam1.targetTexture = MalfuctionTexture;
+            //cam1.enabled = false;   // Caméra coupée
+            CamOutput.texture = malfuctiontexture;
+        }
+        else
+        {
+            //cam1.enabled = true;    // Caméra active
+            cam1.targetTexture = CamRenderTexture;
+            CamOutput.texture = CamRenderTexture;
+        }
     }
 
 }
